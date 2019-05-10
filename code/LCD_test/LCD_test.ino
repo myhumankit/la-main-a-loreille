7b87d0c760c045d42f4b3846a4a936c328849d21
Julien Lebunetel <julien@lebunetel.com>
2019-05-10 14:24:22 +0200

    Use VMA203 LCD Keypad Shield

--- code/LCD_test/LCD_test.ino
+++ code/LCD_test/LCD_test.ino
@@ -2,22 +2,26 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-#define LCD_RS 7
-#define LCD_EN 6
-#define LCD_D4 2
-#define LCD_D5 3
-#define LCD_D6 4
-#define LCD_D7 5
+#define LCD_RS 8
+#define LCD_EN 9
+#define LCD_D4 4
+#define LCD_D5 5
+#define LCD_D6 6
+#define LCD_D7 7
+#define LCD_Backlight 10
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 void setup() {
+  // set up the backlight to full brightness:
+  pinMode(LCD_Backlight, OUTPUT);
+  analogWrite(LCD_Backlight, 255);
+
   // set up the LCD's number of columns and rows:
-  lcd.begin(8, 2);
-  // Print a message to the LCD.
+  lcd.begin(16, 2);
+
+  // print a message to the LCD.
   lcd.setCursor(0, 0);
-  lcd.print("Hello");
-  lcd.setCursor(0, 1);
-  lcd.print("World !");
+  lcd.print("Hello World !");
 }
 
 void loop() {
